44f82bf10c487766e0e1722d0cdd16e228fda88b
Josh Girvin <josh@studionone.com.au>
2015-11-26 13:53:33 +1000

    Bumping version

--- src/dup.nim
+++ src/dup.nim
@@ -19,7 +19,7 @@ import strutils
 import json
 import docopt
 
-let args = docopt(doc, version = "Docker Up 0.1")
+let args = docopt(doc, version = "Docker Up v0.1.1")
 
 const dupFile = ".up.json"
 const stateFile = ".up.state"

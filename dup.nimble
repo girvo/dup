71a88bfb588353b99567a339eb26ccda373b0f9c
Josh Girvin <josh@studionone.com.au>
2015-11-26 11:24:15 +1000

    Building out base CLI with docopt

---
+++ dup.nimble
@@ -0,0 +1,9 @@
+[Package]
+name          = "dup"
+version       = "0.1.0"
+author        = "Josh Girvin <josh@jgirvin.com>"
+description   = "CLI wrapper for local Docker web development"
+license       = "MIT"
+
+[Deps]
+Requires: "nim >= 0.10.0, docopt >= 0.6.2"
--- src/dup.nim
+++ src/dup.nim
@@ -1,3 +1,28 @@
-import os
+let doc = """
+Declaratively define and run stateful Docker containers for web development.
 
-echo("Hello, world!")
+Usage:
+  dup up
+  dup down
+  dup init
+  dup status
+  dup build [--no-cache]
+  dup (-h | --help)
+  dup --version
+"""
+
+import os
+import strutils
+import docopt
+
+let args = docopt(doc, version = "Docker Up 0.1")
+
+const dupFile = ".up.json"
+
+proc checkDupFile() =
+  let currDir = getCurrentDir()
+  if not existsFile(currDir / dupFile):
+    echo("Missing \".up.json\" in current directory.")
+    quit(1)
+
+checkDupFile()

8127c855905f91c8033ff135b16075543c68a03b
Josh Girvin <josh@studionone.com.au>
2015-11-26 13:12:47 +1000

    State file now appends itself to gitignore, fixed bug in MySQL creation

--- src/dup.nim
+++ src/dup.nim
@@ -47,7 +47,37 @@ proc checkStatefile(): bool =
   return existsFile(getCurrentDir() / stateFile)
 
 proc buildStatefile() =
+  echo("Building .up.state file...")
   open(getCurrentDir() / stateFile, fmWrite).close()
+  if existsFile(getCurrentDir() / ".gitignore"):
+    var
+      hasLine: bool = false
+      o = open(getCurrentDir() / ".gitignore", fmRead)
+
+    for line in o.lines:
+      if line == ".up.state":
+        hasLine = true
+    o.close()
+
+    if not hasLine:
+      echo("Appending .up.state to .gitignore...")
+      var a = open(getCurrentDir() / ".gitignore", fmAppend)
+      a.writeLine(".up.state")
+      a.close()
+
+proc startMysql(project: string, dbname: string, dbpass: string) =
+  echo "Starting MySQL..."
+  let command = "docker run -d --name " & project & "-db --volumes-from " & project & "-data -p 3306:3306 -e MYSQL_PASS=" & dbpass & " -e ON_CREATE_DB=" & dbname & " tutum/mysql"
+  let exitCode = execCmd command
+  if exitCode != 0:
+    echo("Error: Starting MySQL failed. Check the output above.")
+
+proc startWeb(project: string) =
+  echo "\nStarting web server..."
+  let command = "docker run -d --name " & project & "-web -p 80:80 -v $(pwd)/code:/var/www --link " & project & "-db:db " & project & ":latest"
+  let exitCode = execCmd command
+  if exitCode != 0:
+    echo("Error: Starting web server failed. Check the output above.")
 
 # Check our Dockerfile and .up.json files exist
 checkDockerfile()
@@ -57,7 +87,7 @@ let config = checkDupFile()
 ###
 # Command definitions
 ##
-
+##
 if args["init"]:
   if checkStateFile():
     echo("Error: Docker Up has already been initalised.")
@@ -78,7 +108,6 @@ if args["init"]:
     else:
       echo("An error occurred!\n\nSee the following for details:\n" & output)
       quit(exitCode)
-
   else:
     echo("Error: Invalid database type specified in config.")
     quit(252)
@@ -87,9 +116,25 @@ if args["init"]:
 if args["up"]:
   if not checkStatefile():
     quit(252)
+  case config["db"]["type"].getStr():
+  of "mysql":
+    startMysql(config["project"].getStr(), config["db"]["name"].getStr(), config["db"]["pass"].getStr())
+    startWeb(config["project"].getStr())
+  else:
+    echo("Not implemented yet.")
+    quit(252)
   quit(0)
 
 if args["down"]:
   if not checkStatefile():
     quit(252)
+  quit(0)
+
+if args["build"]:
+  echo("Building latest image...")
+  let command = "docker build -t " & config["project"].getStr() & ":latest ."
+  let exitCode = execCmd(command)
+  if exitCode != 0:
+    quit(exitCode)
+  echo("Done.")
   quit(0)

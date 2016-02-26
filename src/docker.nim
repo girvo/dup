6c7cd1df987f1f2676f5a118386a41ea898ca94b
Josh Girvin <josh@studionone.com.au>
2016-02-26 16:47:31 +1000

    Removing binary test runner

--- src/docker.nim
+++ src/docker.nim
@@ -1,9 +1,18 @@
 import os
+import net
+import uri
 
-proc getHost*(k: string = "DOCKER_HOST"): string =
+# TODO: This needs to be cross-platform and cross-host
+# TODO: This also needs to be configurable
+proc getHost*(k: string = "DOCKER_HOST") =
     var host = ""
     if existsEnv(k):
         host = getEnv(k)
     else:
         host = "unix:///var/run/docker.sock"
-    return host
+    var parsed = parseUri(host)
+    echo($parsed)
+
+# proc connect*(host: DockerHost) =
+#     var sock = newSocket($host)
+#     var conn = sock.connect()
--- tests/docker_test.nim
+++ tests/docker_test.nim
@@ -9,8 +9,6 @@ import os
 suite "docker":
     test "getHost pulls DOCKER_HOST if available":
         var hostKey = "FAKE_DOCKER_HOST"
-
-        # Temporarily putEnv for our fake docker host
         os.putEnv(hostKey, "testing")
         var res = docker.getHost(hostKey)
         os.putEnv(hostKey, "")

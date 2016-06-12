62b4293b78abc5425f866ea96a03c2641f33d4e2
Josh Girvin <josh@jgirvin.com>
2016-06-13 00:08:24 +1000

    Cleaning up names

--- src/core.nim
+++ src/core.nim
@@ -4,9 +4,9 @@ import os, strutils, future
 import fp/option
 import docker
 
-proc hello*() : void =
-  var theThing: Option[string] = Some("testing")
-  echo theThing.map((input: string) -> string =>
+proc hello*() =
+  var testing = Some("testing")
+  echo testing.map((input: string) -> string =>
     input & "!!").map((input: string) -> string =>
       input & "??")
-  echo get theThing
+  echo get testing

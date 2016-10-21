42e9ead986fcc1a6005dbbceb9c3a78384e7cd6c
Nathan Craike <nathan@studionone.com.au>
2016-10-21 10:34:14 +1000

    Sets version 1.0.1 in one more place.

--- src/dup.nim
+++ src/dup.nim
@@ -13,7 +13,7 @@ from database import newDBConfig
 from container import checkDockerfile, checkAndParseDupFile
 
 ## Define our version constant for re-use
-const version = "dup 1.0.0"
+const version = "dup 1.0.1"
 
 ## Define our docopt parsing schema
 let doc = """

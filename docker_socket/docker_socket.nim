ae8ba2526d283e8ec1a9f21932df420bbd0860ab
Josh Girvin <josh@jgirvin.com>
2017-03-07 23:39:09 +1000

    Adding an HTTP parser to Nim for Docker handling
    This works via AF_UNIX sockets, and is a proper state-machine based implementation of an HTTP parser, in pure Nim

--- docker_socket/docker_socket.nim
+++ docker_socket/docker_socket.nim
@@ -1,3 +1,73 @@
 ## A Docker API implementation via Unix sockets
 
-echo "Hello, World"
+import
+  net,
+  nre,
+  strutils,
+  sequtils
+
+type
+  ContentState {.pure.} = enum
+    Text, Gzip
+  BodyState {.pure.} = enum
+    Unknown, ContentLength, Chunked
+  ParserState {.pure.} = enum
+    Headers, Body
+  HttpParser = object
+    currentState: ParserState
+    bodyState: BodyState
+    contentState: ContentState
+
+proc newHttpParser(): HttpParser =
+  result.bodyState = BodyState.Unknown
+  result.currentState = ParserState.Headers
+  result.contentState = ContentState.Text
+
+var
+  parser: HttpParser = newHttpParser()
+const
+  dockerSocket = "/var/run/docker.sock"
+let
+  sock: Socket = newSocket(AF_UNIX, SOCK_STREAM, IPPROTO_IP)
+  # handle: SocketHandle = sock.getFd()
+# handle.setBlocking(false)
+
+echo "Connecting to socket..."
+sock.connectUnix(dockerSocket)
+sock.send("GET /v1.26/containers/json HTTP/1.1\nHost: localhost\n\n")
+var
+  linebuf: TaintedString = ""
+  bodybuf: TaintedString = ""
+  lines = newSeq[TaintedString]()
+  bodylen: int = 0
+let
+  contentLength = re"^Content\-Length\:\ ?([\d]+)$"
+  transferChunked = re"^Transfer-Encoding: chunked$"
+# Read headers
+while parser.currentState == ParserState.Headers:
+  sock.readLine(linebuf)
+  if linebuf != "\r\n":
+    lines.add(linebuf)
+
+    ### TODO: Move this into a parseHeaders that iterates over the seq instead
+    var contentLengthMatch = linebuf.find(contentLength)
+    if contentLengthMatch.isSome:
+      parser.bodyState = BodyState.ContentLength
+      bodylen = parseInt(contentLengthMatch.get().captures[0])
+    if linebuf.contains(transferChunked):
+      parser.bodyState = BodyState.Chunked
+  else:
+    parser.currentState = ParserState.Body
+
+# Read body
+if parser.bodyState == BodyState.ContentLength:
+  bodybuf.setLen(bodylen)
+  discard sock.recv(bodybuf, bodylen)
+  lines.add(bodybuf)
+  echo bodybuf.strip()
+elif parser.bodyState == BodyState.Chunked:
+  echo "Transfer encoding, chunked!!!"
+else:
+  echo "Unknown state for HttpParser.bodyState"
+
+# for line in lines: echo "'", line, "'"

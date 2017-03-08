92550385f55f1ff9f72b99463bc02dab7b5aae2d
Josh Girvin <josh@jgirvin.com>
2017-03-08 16:36:19 +1000

    Uses PEGs, proper methods, state machine
    Moved away from `nre` to `pegs` for nicer parsing, can probably combine all the parsers into a single one in the future.
    
    Moved to a proper OOP-like structure, operating on an `HttpParser` object with methods and state.
    
    Can't make the parser unaware of the transport, as HTTP requires knowing how to request the next set of data (given a particular length, etc.) -- perhaps I can acheive this via procvars/dynamic dispatch?

--- docker_socket/docker_socket.nim
+++ docker_socket/docker_socket.nim
@@ -1,70 +1,131 @@
 ## A Docker API implementation via Unix sockets
 
-import
-  net,
-  nre,
-  strutils,
-  sequtils
+import net, pegs, unicode, sequtils
+from strutils import strip, parseInt, join, splitLines
+
+## PEGs for parsing headers and status lines
+let
+  httpHeaderPeg = peg"""
+    header <- {key} \s* ':' \s* {value+}
+    key <- [A-Za-z0-9_-]*
+    value <- \S*\ident*[;\ ]*
+  """
+  statusLinePeg = peg"i'http/1.'[01] \s+ {.*}"
 
 type
+  ## HTTP Response types
+  Header = tuple[key, value: string]
+  Response = object of RootObj
+    statusLine*: string
+    statusCode*: int
+    headers: seq[Header]
+    body: string
+    bodyLength*: int
+  ## Parser types
   ContentState {.pure.} = enum
     Text, Gzip
   BodyState {.pure.} = enum
     Unknown, ContentLength, Chunked
   ParserState {.pure.} = enum
     Headers, Body
-  HttpParser = object
+  HttpParser = object of RootObj
     currentState: ParserState
     bodyState: BodyState
     contentState: ContentState
+    rawHeaders: TaintedString
+    curResponse: Response
+
+## Constructors
+proc newEmptyResponse(): Response =
+  result.statusLine = ""
+  result.statusCode = -1
+  result.headers = @[]
+  result.body = ""
+  result.bodyLength = -1
 
 proc newHttpParser(): HttpParser =
   result.bodyState = BodyState.Unknown
   result.currentState = ParserState.Headers
   result.contentState = ContentState.Text
+  result.rawHeaders = ""
+  result.curResponse = newEmptyResponse()
 
-var
-  parser: HttpParser = newHttpParser()
+## Procs and Methods
+proc keyEqualTo(header: Header, cmp: string): bool =
+  header.key.toLower == cmp.toLower
+
+proc parseStatus(raw: string): tuple[code: int, msg: string] =
+  if raw =~ peg"{\d\d\d}\ {.*}":
+    result.code = matches[0].parseInt()
+    result.msg = matches[1]
+  else:
+    raise newException(OSError, "Invalid status code?")
+
+method addHeader(self: var Response, header: Header) {.base.} =
+  # Adds a header tuple to the internal headers seq
+  self.headers.add(header)
+
+method setBody(self: var Response, body: string) {.base.} =
+  # Sets the body string value (TODO: It's not always a string!)
+  self.body = body
+
+method addRawHeader(self: var HttpParser, line: TaintedString) {.base.} =
+  self.rawHeaders.add(line & "\n")
+
+method parseHeaders(self: var HttpParser) {.base.} =
+  ## Parses single header lines into proper Header tuples
+  for line in splitLines(self.rawHeaders):
+    if line =~ httpHeaderPeg:
+      self.curResponse.headers.add((key: matches[0], value: matches[1]))
+    elif line =~ statusLinePeg:
+      let
+        parsed = matches[0].parseStatus()
+      self.curResponse.statusCode = parsed.code
+      self.curResponse.statusLine = line
+
+method readHeaders(self: var HttpParser) {.base.} =
+  ## Read the headers to control teh state machine
+  for i, header in self.curResponse.headers:
+    if header.keyEqualTo("content-length"):
+      self.bodyState = BodyState.ContentLength
+      self.curResponse.bodyLength = header.value.parseInt()
+    elif header.keyEqualTo("transfer-encoding") and header.value == "chunked":
+      self.bodyState = BodyState.Chunked
+
+###
+# Socket handling for Docker
+##
 const
   dockerSocket = "/var/run/docker.sock"
 let
   sock: Socket = newSocket(AF_UNIX, SOCK_STREAM, IPPROTO_IP)
-  # handle: SocketHandle = sock.getFd()
-# handle.setBlocking(false)
+var
+  parser: HttpParser = newHttpParser()
+  linebuf: TaintedString = ""
+  bodybuf: TaintedString = ""
 
+## TODO: This should be ripped out of the parser, parser should work solely on strings
 echo "Connecting to socket..."
 sock.connectUnix(dockerSocket)
 sock.send("GET /v1.26/containers/json HTTP/1.1\nHost: localhost\n\n")
-var
-  linebuf: TaintedString = ""
-  bodybuf: TaintedString = ""
-  lines = newSeq[TaintedString]()
-  bodylen: int = 0
-let
-  contentLength = re"^Content\-Length\:\ ?([\d]+)$"
-  transferChunked = re"^Transfer-Encoding: chunked$"
-# Read headers
+
+# Read raw headers into a string
 while parser.currentState == ParserState.Headers:
   sock.readLine(linebuf)
   if linebuf != "\r\n":
-    lines.add(linebuf)
-
-    ### TODO: Move this into a parseHeaders that iterates over the seq instead
-    var contentLengthMatch = linebuf.find(contentLength)
-    if contentLengthMatch.isSome:
-      parser.bodyState = BodyState.ContentLength
-      bodylen = parseInt(contentLengthMatch.get().captures[0])
-    if linebuf.contains(transferChunked):
-      parser.bodyState = BodyState.Chunked
+    parser.addRawHeader(linebuf)
   else:
     parser.currentState = ParserState.Body
 
+parser.parseHeaders()
+parser.readHeaders()
+
 # Read body
 if parser.bodyState == BodyState.ContentLength:
-  bodybuf.setLen(bodylen)
-  discard sock.recv(bodybuf, bodylen)
-  lines.add(bodybuf)
-  echo bodybuf.strip()
+  bodybuf.setLen(parser.curResponse.bodyLength)
+  discard sock.recv(bodybuf, parser.curResponse.bodyLength)
+  parser.curResponse.body = bodybuf
+  echo parser.curResponse
 elif parser.bodyState == BodyState.Chunked:
   echo "Transfer encoding, chunked!!!"
 else:
